815f78a227e073343220f458388f2298d1911d1f
m2kohand <uh.liamg@gmail.com>
2015-03-12 19:03:42 +0100

    A feladat adott

--- proba_projekt/main.cpp
+++ proba_projekt/main.cpp
@@ -2,8 +2,16 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+
+ //double& operator[](int)
+
+
+int main() {
+    Tomb t(30);
+    std::cout << t[20]; //index op.
+    t[22] = 10.2; // referenciával tér vissza az index [] oerátor
+    t.atmeretez(50); // dinamikus átméretezés
+    Tomb masolat(t), harmadik; //copy constructor
+    harmadik = t; // operator=
 }
+
