febfce3fbb22c18dfaec1d6f26c5830ec0d5bb39
kohlmannmark <kohlmann.mark@gmail.com>
2015-03-16 19:18:18 +0100

    Második commit
    Merthogy miért ne legyen darabokban.

--- proba_projekt/main.cpp
+++ proba_projekt/main.cpp
@@ -2,17 +2,20 @@
 
 using namespace std;
 
-
- //double& operator[](int)
-
-//kosz b+
+ class Tomb {
+    double *items;
+public:
+    Tomb() {items = new double[60];}
+    Tomb (int _size) {items = new double[_size];}
+    void atmeretez (int) {;}
+    double& operator[](const int);
+ };
 
 int main() {
     Tomb t(30);
-    std::cout << t[20]; //index op.
-    t[22] = 10.2; // referenciával tér vissza az index [] oerátor
-    t.atmeretez(50); // dinamikus átméretezés
-    Tomb masolat(t), harmadik; //copy constructor
-    harmadik = t; // operator=
+//    std::cout << t[20]; //index op.
+//    t[22] = 10.2; // referenciával tér vissza az index [] oerátor
+//    t.atmeretez(50); // dinamikus átméretezés
+//    Tomb masolat(t), harmadik; //copy constructor
+//    harmadik = t; // operator=
 }
-
